4699b17b80029cbcc0cb736e83f7cd56ae5412ed
Steven Hall <stevenbryanb.work@gmail.com>
2024-05-14 18:35:20 +0100

    Add XML & fields for MVC

--- Dungeon/Program.cs
+++ Dungeon/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using Dungeon.Items;
 
 namespace Dungeon
 {
+    /// <summary>
+    /// Represents the game entry point.
+    /// 3 Instaces a created: Model, View and Controller
+    /// Finaly the controller is run (Game loop)
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args">Not used</param>
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            // EXAMPLE of HealthPotion
+            Item hp = new HealthPotion( 100 );
+
+            if(hp is HealthPotion)
+            {
+                hp.Interact();
+            }
+
+            
+
+            // Instantiate a model
+
+            // Instantiate a view
+
+            // Instantiate a controller
+
+
+            // Run controller
+        
         }
     }
 }
