363b5020228a6a9e71a4192fccf94b6011e9629b
Steven Hall <stevenbryanb.work@gmail.com>
2024-05-14 17:36:51 +0100

    Add XML to Item class

--- Dungeon/Items/HealthPotion.cs
+++ Dungeon/Items/HealthPotion.cs
@@ -2,18 +2,33 @@ using System;
 
 namespace Dungeon.Items
 {
+    /// <summary>
+    /// Represents Health Potion item
+    /// </summary>
     public class HealthPotion : Item
     {
-        public int Health { get; private set; }
+        // Read-only property
+        public int Health { get; }
 
-
+        /// <summary>
+        /// Constructor for HealthPotion class
+        /// Initializes Health property with the value passed as argument
+        /// </summary>
+        /// <param name="health">
+        /// Value to be assigned to Health property (read-only)
+        /// </param>
         public HealthPotion(int health)
         {
             Health = health;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public override void Interact()
         {
+            // TODO: Remove Console.WriteLine from this class as it violates MVC pattern
+            // TODO: Link with player class and increase player health
             Console.WriteLine("You drink the health potion and gain " + Health + " health.");
         } 
     }
--- Dungeon/Items/Item.cs
+++ Dungeon/Items/Item.cs
@@ -1,9 +1,14 @@
-using System;
-
 namespace Dungeon.Items
 {
+    /// <summary>
+    /// Abstract representation of an object(in-game item) 
+    /// </summary>
     public abstract class Item
     {
+        /// <summary>
+        /// Allows the player to interact with the items.
+        /// Method is overriden in by all items subclasses.
+        /// </summary>
         public virtual void Interact(){}
         
     }
