58039367d1ef9e572e5abd070d926d4099bb5c1a
Steven Hall <stevenbryanb.work@gmail.com>
2024-05-14 17:21:01 +0100

    Add example of interaction for HP Update consists of adding simple Interact logic for HealthPotion class

--- Dungeon/Items/HealthPotion.cs
+++ Dungeon/Items/HealthPotion.cs
@@ -4,9 +4,17 @@ namespace Dungeon.Items
 {
     public class HealthPotion : Item
     {
+        public int Health { get; private set; }
+
+
+        public HealthPotion(int health)
+        {
+            Health = health;
+        }
+
         public override void Interact()
         {
-            // pode dar a vida
+            Console.WriteLine("You drink the health potion and gain " + Health + " health.");
         } 
     }
 }
