271afa7d4861a9a51236747f50d29494e5443198
Steven Hall <stevenbryanb.work@gmail.com>
2024-05-15 22:01:31 +0100

    Add enemy asbtract properties

--- Dungeon/Characters/Enemy.cs
+++ Dungeon/Characters/Enemy.cs
@@ -3,8 +3,16 @@ using System.Collections.Generic;
 
 namespace Dungeon.Characters
 {
+    /// <summary>
+    /// Abstract class representing an enemy in the game
+    /// </summary>
     public abstract class Enemy
     {
-        
+        // Abstract property that represents enemy Health Points
+        public abstract int Hp { get; set; }
+
+
+        // Abstract property that represents enemy Attack Power
+        public abstract int AttackPower { get; }
     }
 }
