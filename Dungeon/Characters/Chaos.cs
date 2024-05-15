1da92a95fd9b32b336f59d58d01f1795917d65c0
Steven Hall <stevenbryanb.work@gmail.com>
2024-05-15 22:03:57 +0100

    Add Auto Implement => Chaos TODO: Add override Properties

--- Dungeon/Characters/Chaos.cs
+++ Dungeon/Characters/Chaos.cs
@@ -7,6 +7,11 @@ namespace Dungeon.Characters
 {
     public class Chaos : Enemy
     {
+        // TODO: Implement the Chaos class 
+
+        public override int Hp { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override int AttackPower => throw new NotImplementedException();
         
     }
 }
