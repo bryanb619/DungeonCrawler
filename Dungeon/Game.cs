b132298cc2708129a3ca3483fa3122df2fbc735e
Steven Hall <stevenbryanb.work@gmail.com>
2024-05-16 18:32:41 +0100

    Change Controller => Game

---
+++ Dungeon/Game.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dungeon
+{
+    public class Game
+    {
+        //private readonly IView _view;
+
+
+        public void Start()
+        {
+            
+        }
+        
+    }
+
+    
+}
