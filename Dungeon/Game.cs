4201a06be823e40a596e7beafab931deec67ee65
Steven Hall <stevenbryanb.work@gmail.com>
2024-05-16 18:35:50 +0100

    Add IView ref, & Start with cycle to Game as Controller

--- Dungeon/Game.cs
+++ Dungeon/Game.cs
@@ -1,17 +1,25 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace Dungeon
 {
     public class Game
     {
-        //private readonly IView _view;
+        // Reference to the view Interface
+        private  IView _view;
 
 
-        public void Start()
+        public void Start(IView view)
         {
+            while(true)
+            {
+                // Run the game loop
+                // Get the player input
+                // Update the model
+                // Update the view
+                // Check for game over
+            }
             
         }
         
