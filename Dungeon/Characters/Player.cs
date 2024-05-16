9418112241de6d6207e3eaf57e2f353b7a5f9bc5
Steven Hall <stevenbryanb.work@gmail.com>
2024-05-16 18:47:29 +0100

    Add Player Basic body

--- Dungeon/Characters/Player.cs
+++ Dungeon/Characters/Player.cs
@@ -5,8 +5,31 @@ using System.Threading.Tasks;
 
 namespace Dungeon.Characters
 {
-    public class Player
+    public class Player 
     {
-        
+        public int Health { get; set; }
+
+        public int AttackPower { get ; set;}
+
+        public Player()
+        {
+            Health = 10000;
+
+        }
+
+        public void Move()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PickUpItem()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Attack()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
