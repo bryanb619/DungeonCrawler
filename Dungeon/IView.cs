fab732f356e5cd0a8b60a999cb26be90ec422ef0
Steven Hall <stevenbryanb.work@gmail.com>
2024-05-16 17:11:21 +0100

    Add Example IView interface

--- Dungeon/IView.cs
+++ Dungeon/IView.cs
@@ -3,8 +3,38 @@ using System.Collections.Generic;
 
 namespace Dungeon
 {
+
+    /// <summary>
+    /// Interface
+    /// </summary>
     public interface IView
     {
+
+        /// <summary>
+        /// Interface method.
+        /// Should be used to display a string message passed as parameter.
+        /// </summary>
+        /// <param name="message">
+        /// Message to be displayed (string)
+        /// /// </param>
+        void DisplayMessage(string message);
+
+
+        /// <summary>
+        /// Interface method.
+        /// Should be used to display a int message passed as parameter.
+        /// </summary>
+        /// <param name="message">
+        /// Message to be displayed (int)
+        /// </param>
+        void DisplayMessage(int message);
+
+
+        /// <summary>
+        /// Interface method.
+        /// Should be used to display final message.
+        /// </summary>
+        void EndMessage();
         
         
     }
