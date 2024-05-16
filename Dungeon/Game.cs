5f194918e52729d753e124460eee621b6f5cf215
Steven Hall <stevenbryanb.work@gmail.com>
2024-05-16 19:09:27 +0100

    Add Example of view usage in Game

--- Dungeon/Game.cs
+++ Dungeon/Game.cs
@@ -12,6 +12,13 @@ namespace Dungeon
 
         public void Start(IView view)
         {
+
+            _view = view;
+
+
+            view.DisplayMessage("Welcome to the dungeon!");
+
+            /*
             while(true)
             {
                 // Run the game loop
@@ -20,6 +27,7 @@ namespace Dungeon
                 // Update the view
                 // Check for game over
             }
+            */
             
         }
         
