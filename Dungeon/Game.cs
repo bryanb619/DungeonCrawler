2fa7f06893b94bf3d778c4d0dd6cea25b462fac7
Steven Hall <stevenbryanb.work@gmail.com>
2024-05-18 18:54:52 +0100

    Add While loop to game

--- Dungeon/Game.cs
+++ Dungeon/Game.cs
@@ -20,28 +20,75 @@ namespace Dungeon
 
         public void Start(IView view)
         {
+            
+
 
             _view = view;
-
             _model.GenerateGame();
 
 
-            view.DisplayMessage("Welcome to the dungeon!");
+            
 
-        
-            /*
+            //if (_model.canMove("E")) view.DisplayMessage("You moved East");
+
+            //else view.DisplayMessage("You can't move there");
+
+
+            view.WelcomeMessage();
+
+            view.ShowMenu();
+
+
             while(true)
             {
+                int option = _view.ShowMenu();
+
+
+                switch(option)
+                {
+                    case 1:
+                        Move();
+                        break;
+                    case 2:
+                        _view.DisplayMessage("You attacked");
+                        break;
+                    case 3:
+                        _view.DisplayMessage("You used an item");
+                        break;
+                    case 4:
+                        _view.DisplayMessage("You quit the game");
+                        break;
+                    default:
+                        _view.DisplayMessage("Invalid option");
+                        break;
+                }
+                
                 // Run the game loop
                 // Get the player input
+               
                 // Update the model
                 // Update the view
                 // Check for game over
             }
-            */
             
         }
-        
+
+
+        private void Move()
+        {
+            string input = "";
+
+            if( _model.canMove(input))
+            {
+                // TODO: add logic for saying where you moved to...
+                _view.DisplayMessage(input);
+
+            } 
+
+            else _view.DisplayMessage("You can't go there...");
+            
+        }
+
     }
 
     
