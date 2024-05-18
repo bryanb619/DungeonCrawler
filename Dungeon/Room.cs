fc2633ebab7ed32eb9b3c4d110689b44b9c6c5a0
Steven Hall <stevenbryanb.work@gmail.com>
2024-05-18 17:13:55 +0100

    Add room base logic Room class is now only responsible for representing a room

--- Dungeon/Room.cs
+++ Dungeon/Room.cs
@@ -1,8 +1,8 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
 using Dungeon.Items;
 using Dungeon.Characters;
+using System.IO;
 
 
 namespace Dungeon
@@ -17,68 +17,26 @@ namespace Dungeon
         // Room properties
         public string Description { get; }
 
-        public Dictionary<string, Room> Connections { get;  }
+        public Dictionary<string, Room> Connections { get; private set;}
 
         public Item Item { get; }
 
         public Enemy Enemy { get; }
+        
 
-        public Room(string description, Dictionary<string, Room> exits, Item item, Enemy enemy)
+        public Room(string description, Item item, Enemy enemy)
         {
 
             // string name, string description, List<Char> exits, Item item, Enemy enemy
-            Description = description;
-            Connections = exits;
-            Item = item;
-            Enemy = enemy;
+            Description     = description;
+            Item            = item;
+            Enemy           = enemy;
         }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        private void LoadRoom()
+        public void AddConnection(Dictionary<string, Room> exits)
         {
-            {
-                try
-                {
-                    using StreamReader sr = new StreamReader("Dungeon/Rooms/Map.txt");
-
-                    // line of file
-                    string line;
-
-                    // Room Loop index
-                    //int i = 0;
-
-                    while ((line = sr.ReadLine()) != null)
-                    {
-                        // read lines of file
-
-
-                        // detect if line has Tab
-                        if(line.Contains("\t"))
-                        {
-                            // add details to room
-                        }
-
-                        else
-                        {
-                            
-                            // add room to list?
-                            //_rooms.Add(new Room());
-                            
-                            // increment index
-
-                        }
-                    }
-                }
-
-                catch (Exception)
-                {
-                    
-
-                }
-               
-            }
+            // Add a connection to the room
+            Connections     = exits;
         }
     }
 }
