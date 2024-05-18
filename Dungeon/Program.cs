c20653c0a9b82a5abe881afb893e447768c83743
Steven Hall <stevenbryanb.work@gmail.com>
2024-05-18 17:40:04 +0100

    Fix MVC Pattern XML & better comments in Program

--- Dungeon/Program.cs
+++ Dungeon/Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using Dungeon.Items;
-using Dungeon.Characters;
-
-namespace Dungeon
+﻿namespace Dungeon
 {
     /// <summary>
     /// Represents the game entry point.
@@ -12,36 +7,31 @@ namespace Dungeon
     /// </summary>
     class Program
     {
-        // DEBUG ROOM
-        private static Room[] room = new Room[15];
 
         /// <summary>
         /// Entry point of the game.
         /// 3 instances are created: Model, Controller and View.
-        /// Model       = Room class => represents Program logic. 
+        /// Model       = model class => represents Game logic. 
         /// Controller  = Game class => represents the game loop.
         /// View        = View class => represents the user interface (UI).
         /// </summary>
         /// <param name="args">Not used</param>
         private static void Main(string[] args)
         {
-
-            // Instantiate a room   (model)
+            // Instantiate a Model              (model)
             Model model = new Model();
 
-    
-            // Instantiate a game   (controller) 
+            // Instantiate a game               (controller) 
             Game game = new Game(model);
 
-            // Instantiate a view 
-            //pass as param (game, model) 
+            // Instantiate a view               (view)
+            //pass as param  (game, model) 
             IView view = new View(game, model);
             
-            // Run the game         
+            // Run the game    
+            // passing as param (view)     
             game.Start(view);
-
-
         }
-
     }
 }
+// -----------------------------------------------------------------------------
