9cfae681acd3326d38e100a149cd6e3807a36501
Steven Hall <stevenbryanb.work@gmail.com>
2024-05-21 20:06:51 +0100

    Add base for XML (ICharacter)

--- Dungeon/Characters/ICharacter.cs
+++ Dungeon/Characters/ICharacter.cs
@@ -2,14 +2,35 @@ namespace Dungeon.Characters
 {
     public interface ICharacter
     {
-        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <value></value>
         int Hp { get; set; }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <value></value>
         int AttackPower { get; set; }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
         void Attack(ICharacter target);
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="amount"></param>
         void Heal(int amount);
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="amount"></param>
         void TakeDamage(int amount);
 
     }
