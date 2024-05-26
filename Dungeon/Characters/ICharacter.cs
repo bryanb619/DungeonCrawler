7d0a1f6dffef885827125b0553f0a7c91be126eb
Steven Hall <stevenbryanb.work@gmail.com>
2024-05-26 22:47:09 +0100

    Add Xml

--- Dungeon/Characters/ICharacter.cs
+++ Dungeon/Characters/ICharacter.cs
@@ -3,38 +3,33 @@ namespace Dungeon.Characters
     public interface ICharacter
     {
         /// <summary>
-        /// 
+        /// Hp property
         /// </summary>
         /// <value></value>
         int Hp { get; set; }
 
 
         /// <summary>
-        /// 
+        /// Attack power property
         /// </summary>
         /// <value></value>
         int AttackPower { get; set; }
 
         /// <summary>
-        /// 
+        /// Receives an ICharacter object as a parameter and attacks it
         /// </summary>
         /// <param name="target"></param>
         void Attack(ICharacter target);
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="amount"></param>
-        void Heal(int amount);
 
         /// <summary>
-        /// 
+        /// Take damage method
         /// </summary>
         /// <param name="amount"></param>
         void TakeDamage(int amount);
 
         /// <summary>
-        /// 
+        /// Detect if the character is dead
         /// </summary>
         /// <returns></returns>
         bool Dead();
--- Dungeon/Characters/Player.cs
+++ Dungeon/Characters/Player.cs
@@ -7,7 +7,6 @@ namespace Dungeon.Characters
     public class Player : ICharacter
     {
 
-
         private int Max_Hp = 1000;
 
         /// <summary>
@@ -18,13 +17,7 @@ namespace Dungeon.Characters
         /// Passed value will be subtracted from the current agent Hp 
         /// Use of the property without setting a value will return the current Hp value of Traveler agent
         /// </value>
-        public int Hp {
-
-            get;
-
-            set;
-
-        }
+        public int Hp { get; set; }
 
         /// <summary>
         /// AttackPower property for the Traveler enemy
@@ -33,11 +26,15 @@ namespace Dungeon.Characters
         /// <value></value>
         public int AttackPower { get ; set; }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <value></value>
         public int PlayerPos { get; private set; } = 0;
 
 
         /// <summary>
-        /// Constructor for the Traveler Enemy
+        /// Constructor for player
         /// </summary>
         public Player()
         {
@@ -45,6 +42,10 @@ namespace Dungeon.Characters
             AttackPower     = 85;
         }
 
+        /// <summary>
+        /// Accepts an ICharacter object as a parameter and attacks it
+        /// </summary>
+        /// <param name="target"></param>
         public void Attack(ICharacter target)
         {
             target.TakeDamage(AttackPower);
@@ -52,7 +53,7 @@ namespace Dungeon.Characters
 
 
         /// <summary>
-        /// 
+        /// Pick up an item
         /// </summary>
         /// <param name="item"></param>
         public void PickUpItem(Item item)
@@ -74,8 +75,11 @@ namespace Dungeon.Characters
             }
                 
         }
-    
 
+        /// <summary>
+        /// Updates player position in world space
+        /// </summary>
+        /// <param name="pos"></param>
         public void UpdatePos(int pos)
         {
             // TODO: Implement movement
@@ -83,7 +87,11 @@ namespace Dungeon.Characters
             
         }
 
-
+        /// <summary>
+        /// Heal the player by a certain amount
+        /// if healing amount exceeds the max hp, set hp to max hp
+        /// </summary>
+        /// <param name="amount">amout of health</param>
         public void Heal(int amount)
         {
             if (Hp + amount > Max_Hp) Hp = Max_Hp;
